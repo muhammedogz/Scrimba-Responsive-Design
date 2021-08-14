0e77bfeddcdd1f400678c0bc6136077b8b386033
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-08-14 14:38:18 +0300

    Update javasciript with esversion-6 info

--- Responsive-Design/Taking-It-Next-Level/Flexbox/script.js
+++ Responsive-Design/Taking-It-Next-Level/Flexbox/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const nav = document.querySelector("nav");
 const button = document.querySelector(".close-nav-btn");
 
@@ -5,13 +7,12 @@ button.addEventListener('click', () => {
     if (nav.classList.contains("nav-mobile-closed"))
     {
         nav.classList.remove("nav-mobile-closed");
-        button.classList.remove("fixed")
+        button.classList.remove("fixed");
     }
     else
     {
         nav.classList.add("nav-mobile-closed");
         button.classList.add("fixed");
     }
-
-})
+});
 
