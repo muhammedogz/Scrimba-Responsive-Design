c34b88fcdfbac5e0a0006e1d17cdbcbdec9731ec
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-08-14 14:27:40 +0300

    Create navigation script

---
+++ Responsive-Design/Taking-It-Next-Level/Flexbox/script.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector("nav");
+const button = document.querySelector(".close-nav-btn");
+
+button.addEventListener('click', () => {
+    if (nav.classList.contains("nav-mobile-closed"))
+    {
+        nav.classList.remove("nav-mobile-closed");
+        button.classList.remove("fixed")
+    }
+    else
+    {
+        nav.classList.add("nav-mobile-closed");
+        button.classList.add("fixed");
+    }
+
+})
+
